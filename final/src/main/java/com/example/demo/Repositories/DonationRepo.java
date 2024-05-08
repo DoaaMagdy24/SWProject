b23428e0036ce4ac3d30944e3145face8bb5e3bb
DoaaMagdy24 <105148032+DoaaMagdy24@users.noreply.github.com>
2024-05-08 12:01:04 +0300

    Delete final directory

--- final/src/main/java/com/example/demo/Repositories/DonationRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Repositories;
-
-import com.example.demo.Entities.Donations;
-import com.example.demo.Entities.Transfer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DonationRepo extends JpaRepository<Donations,Long> {
-}
--- final/src/main/java/com/example/demo/Repositories/TransferRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Repositories;
-
-import com.example.demo.Entities.Transfer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransferRepo extends JpaRepository<Transfer,Long> {
-}
